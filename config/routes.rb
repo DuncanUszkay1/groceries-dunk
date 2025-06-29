c8b05592ae61f1cf8135b0a919b24edd460d3d7b
Sleeping Dragon <sleepingdragon@sleepings-mbp.lan>
2025-06-29 13:52:23 -0400

    Remove headers and route to grocery list by default

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,6 @@ Rails.application.routes.draw do
   post 'list_item/mark_all_as_purchased'
   post 'list_item/delete_all'
   get 'list_item/all'
-  resources :widgets
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-
-  root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'list_item#all'
 end
